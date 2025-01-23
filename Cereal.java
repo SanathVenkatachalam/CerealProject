eae01fa5f2868bdf7a1e14dbdf48f1e667e6b6d5
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2025-01-23 13:50:52 -0800

    first commit

---
+++ Cereal.java
@@ -0,0 +1,28 @@
+public class Cereal{
+    static int cerealCount = 0;
+
+    public Cereal(){
+        String mostNutritious;
+        double percentageCovered;
+        int calorieCount;
+        int volumeCount;
+        String brand;
+        String brandName;
+    }
+
+    public String toString(){
+        return "Cereal";
+    }
+    
+    public static void main(String[] args) {
+        Cereal cereal = new Cereal();
+        cereal.toString();
+    
+        
+
+        
+    }
+}
+
+
+
